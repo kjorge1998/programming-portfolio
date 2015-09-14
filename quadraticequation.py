39217441eecd78081e418b7a1d152f86c08e4ffc
Keaton Jorgensen <keatonjorgensen@Alveyworld-25.local>
2015-09-14 09:49:28 -0600

    practice program

--- quadraticequation.py
+++ quadraticequation.py
@@ -7,4 +7,7 @@ if d < 0
 	print "ROOTS are imaginary"
 else:
 	root1 = (-b + math.sqrt(d)) / (2.0 * a)
-	root2 = (-b - math.sqrt(d))
+	root2 = (-b - math.sqrt(d)) / (2.0 * a)
+	print "Root 1 = ", root1
+	print "Root 2 = ", root2
+	
